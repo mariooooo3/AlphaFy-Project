85cfe055b3770d921c7a3b266b9593a3c8131c7e
mariooooo3 <afrasineimario@gmail.com>
2026-01-14 03:25:01 +0200

    main update

--- src/AlphaFy/AlphaFy.java
+++ src/AlphaFy/AlphaFy.java
@@ -1,24 +1,28 @@
 package AlphaFy;
+
 import GUI.Interface;
+
 import javax.swing.SwingUtilities;
 
-
 public class AlphaFy {
-    String userName;
+
+    private final String userName;
 
     public AlphaFy(String userName) {
         this.userName = userName;
-        System.out.println("Welcome back " + this.userName);
+    }
+
+    public void start() {
+        Interface ui = new Interface(this.userName);
+        ui.setLocationRelativeTo(null);
+        ui.setVisible(true);
     }
 
     public static void main(String[] args) {
-        AlphaFy alphaFy = new AlphaFy("mariooo&clap");
-
         SwingUtilities.invokeLater(() -> {
-            Interface ui = new Interface("mariooo&clap");
-            ui.setVisible(true);
+            AlphaFy app = new AlphaFy("mariooo&clap");
+            app.start();
         });
-
     }
-
 }
+
